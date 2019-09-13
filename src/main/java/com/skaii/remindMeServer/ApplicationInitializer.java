e0b7bc43775dcbd30f3339bcca72eecca563e27f
Igor Zolov <daledreams@gmail.com>
2019-09-13 15:43:59 +0300

    Create base structure

---
+++ src/main/java/com/skaii/remindMeServer/ApplicationInitializer.java
@@ -0,0 +1,28 @@
+package com.skaii.remindMeServer;
+
+import com.skaii.remindMeServer.config.WebConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.ServletRegistration.Dynamic;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+
+    private final static String DISPATCHER = "dispatcher";
+
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+        servletContext.addListener(new ContextLoaderListener(context));
+
+        ServletRegistration.Dynamic servlet = servletContext.addServlet(DISPATCHER, new DispatcherServlet(context));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
---
+++ src/main/java/com/skaii/remindMeServer/controller/ReminderController.java
@@ -0,0 +1,20 @@
+package com.skaii.remindMeServer.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/reminder")
+public class ReminderController {
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getReminder(ModelMap model){
+        return "Test remind";
+    }
+
+}
